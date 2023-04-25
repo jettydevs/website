6316f645d77d799b39a6ba5e5b4c0f88f483cb67
wzalazar <walter.zalazar.mdp@gmail.com>
2023-04-25 14:19:38 -0300

    fix: analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,19 +37,6 @@ module.exports = {
         trackingIds: [
           "G-JG5LX8W64V", // Google Analytics / GA
         ],
-        // This object is used for configuration specific to this plugin
-        pluginConfig: {
-          // Puts tracking script in the head instead of the body
-          head: false,
-          // Setting this parameter is also optional
-          respectDNT: true,
-          // Avoids sending pageview hits from custom paths
-          exclude: ["/preview/**", "/do-not-track/me/too/"],
-          // Defaults to https://www.googletagmanager.com
-          origin: "https://jettydevs.com/",
-          // Delays processing pageview events on route update (in milliseconds)
-          delayOnRouteUpdate: 0,
-        },
       },
     },
   ],

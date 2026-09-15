174fcfdbe372cbba6463c3bd37fb3bd8050b1cc1
Walter Zalazar <walter.zalazar.mdp@gmail.com>
2026-09-15 11:09:07 -0300

    remove Walter profile (#11)

--- src/components/OurTeam.js
+++ src/components/OurTeam.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 
-import walter from "../images/team/walter.png"
 import jose from "../images/team/jose.png"
 
 const OurTeam = () => {
@@ -10,50 +9,10 @@ const OurTeam = () => {
         <div className="container">
           <div className="consulting-section-title">
             <span>ABOUT US</span>
-            <h2>Meet Our Co-Founders</h2>
+            <h2>Meet Our Founder</h2>
           </div>
 
           <div className="row justify-content-center">
-            <div
-              className="col-lg-4 col-md-6"
-              data-aos="fade-up"
-              data-aos-duration="1200"
-              data-aos-delay="200"
-              data-aos-once="true"
-            >
-              <div className="consulting-team-card">
-                <img src={walter} alt="Walter" />
-                <div className="team-content">
-                  <h3>Walter Zalazar</h3>
-                  <p>Co-Founder</p>
-                </div>
-                <div className="social-links">
-                  <ul>
-                    {/* <li>
-                      <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
-                        <i className="flaticon-facebook-1"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="https://www.twitter.com" target="_blank" rel="noreferrer">
-                        <i className="flaticon-twitter-3"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="https://google.com" target="_blank" rel="noreferrer">
-                        <i className="flaticon-google-plus"></i>
-                      </a>
-                    </li> */}
-                    <li>
-                      <a href="https://www.linkedin.com/in/wzalazar/" target="_blank" rel="noreferrer">
-                        <i className="flaticon-linkedin-1"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-
             <div
               className="col-lg-4 col-md-6"
               data-aos="fade-up"
@@ -65,7 +24,7 @@ const OurTeam = () => {
                 <img src={jose} alt="Jose" />
                 <div className="team-content">
                   <h3>José Casella</h3>
-                  <p>Co-Founder</p>
+                  <p>Founder</p>
                 </div>
                 <div className="social-links">
                   <ul>
--- src/components/TeamMember.js
+++ src/components/TeamMember.js
@@ -1,6 +1,5 @@
 import * as React from "react"
 
-import walter from "../images/team/walter.png"
 import jose from "../images/team/jose.png"
 
 const TeamMember = () => {
@@ -21,63 +20,11 @@ const TeamMember = () => {
             <div className="col-lg-4 col-md-6">
               <div className="currency-team-card">
                 <div className="team-image">
-                  <img src={walter} alt="Walter" />
-                </div>
-                <div className="team-content">
-                  <h3>Walter Zalazar</h3>
-                  <p>Co-Founder</p>
-                  <div className="social-links">
-                    <ul>
-                      {/* <li>
-                        <a
-                          href="https://www.facebook.com"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="flaticon-facebook-1"></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="https://www.twitter.com"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="flaticon-twitter-3"></i>
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="https://google.com"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="flaticon-google-plus"></i>
-                        </a>
-                      </li> */}
-                      <li>
-                        <a
-                          href="https://www.linkedin.com/in/wzalazar/"
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <i className="flaticon-linkedin-1"></i>
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 col-md-6">
-              <div className="currency-team-card">
-                <div className="team-image">
                   <img src={jose} alt="Jose" />
                 </div>
                 <div className="team-content">
                   <h3>José Casella</h3>
-                  <p>Co-Founder</p>
+                  <p>Founder</p>
                   <div className="social-links">
                     <ul>
                       {/* <li>
